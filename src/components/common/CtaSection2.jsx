c42c688ebc3c1f7a8d09f1c925dbf3540b97e804
tahab-cp <taha.b@cp.agency>
2025-10-03 12:53:43 +0500

    cta section 2 book button updated

--- src/components/common/CtaSection2.jsx
+++ src/components/common/CtaSection2.jsx
@@ -7,6 +7,8 @@ import FlashIcon from "@/assets/icons/3d-flash-icon.svg";
 import ContactArrowIcon from "@/assets/icons/contact-arrow.svg";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
+import RightArrowIcon from "@/assets/icons/right-arrow-icon.svg";
+import { getCalApi } from "@calcom/embed-react";
 
 const CtaSection2 = () => {
   const containerRef = useRef();
@@ -22,6 +24,21 @@ const CtaSection2 = () => {
         toggleActions: "play none none none",
       },
     });
+  }, []);
+
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: "15min" });
+      cal("ui", {
+        theme: "dark",
+        cssVarsPerTheme: {
+          light: { "cal-brand": "#292929" },
+          dark: { "cal-brand": "#FF37B3" },
+        },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
   }, []);
 
   return (
@@ -66,7 +83,53 @@ const CtaSection2 = () => {
               Book a FREE strategy call with Hassan.
             </h4>
 
-            <CommonBtn3 href="/contact" label="Book a Call" bgColor="#FF37B3" />
+            <button
+              data-cal-namespace="15min"
+              data-cal-link="hassan-iqbal-mznzu9/15min"
+              data-cal-config='{"layout":"month_view","theme":"dark"}'
+              className="group inline-flex cursor-pointer items-center"
+            >
+              {/* Text */}
+              <div className="relative z-[2] h-[4rem] min-w-max overflow-hidden rounded-[6rem] bg-[#FF37B3] px-[2rem] text-[1.6rem] font-semibold text-white md:h-[5rem] md:px-[3rem] md:text-[2rem]">
+                <span className="flex size-full items-center justify-center transition-all duration-200 group-hover:-translate-y-full">
+                  Book a Call
+                </span>
+
+                <span className="flex size-full items-center justify-center transition-all duration-200 group-hover:-translate-y-full">
+                  Book a Call
+                </span>
+              </div>
+
+              {/* Wrapper (animated arrow) */}
+              <div className="relative flex items-center justify-center">
+                {/* Subtract Icon */}
+                <i className="mx-[-.5rem]">
+                  <svg
+                    width="19"
+                    height="18"
+                    viewBox="0 0 19 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M2.47559 0C3.85901 2.39049 6.44175 4 9.40234 4C12.3629 4 14.9457 2.39049 16.3291 0H18.4023V18H16.3291C14.9457 15.6095 12.3629 14 9.40234 14C6.44175 14 3.85901 15.6095 2.47559 18H0.402344V0H2.47559Z"
+                      fill="#FF37B3"
+                    />
+                  </svg>
+                </i>
+
+                {/* Arrow Icon */}
+                <div className="relative size-[4rem] overflow-hidden rounded-full bg-[#FF37B3] md:size-[5rem]">
+                  <i className="absolute top-0 left-0 flex size-full items-center justify-center transition-all duration-200 group-hover:left-full">
+                    <RightArrowIcon className="fill-white" />
+                  </i>
+
+                  <i className="absolute top-0 -left-full flex size-full items-center justify-center transition-all duration-200 group-hover:-left-0">
+                    <RightArrowIcon className="fill-white" />
+                  </i>
+                </div>
+              </div>
+            </button>
           </div>
         </div>
       </div>
