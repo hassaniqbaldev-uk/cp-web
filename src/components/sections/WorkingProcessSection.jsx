54f647c592e3f4670226daf42079166cf7839a33
tahab-cp <taha.b@cp.agency>
2025-10-07 12:08:00 +0500

    working process animation added

--- src/components/sections/WorkingProcessSection.jsx
+++ src/components/sections/WorkingProcessSection.jsx
@@ -14,31 +14,183 @@ import LineStroke25 from "@/assets/decorative-elements/line-stroke-25.svg";
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { SplitText } from "gsap/all";
 
 const WorkingProcessSection = () => {
   const labelRef = useRef();
+  const container = useRef();
 
-  useGSAP(() => {
-    // Wobble/shake animation
-    gsap.to(labelRef.current, {
-      rotation: "+=3", // Rotate 3 degrees back and forth
-      duration: 0.15, // Very short duration for quick wobble
-      yoyo: true, // Go back and forth
-      repeat: -1, // Infinite repeat
-      ease: "sine.inOut", // Best ease for wobble effects
-      repeatDelay: 0.5, // Small pause between wobbles
-    });
-  });
+  useGSAP(
+    () => {
+      const splitHeading = new SplitText(
+        container.current.querySelector(".working-process-heading"),
+        {
+          type: "lines",
+          linesClass: "line",
+        },
+      );
+      const splitDesc1 = new SplitText(
+        container.current.querySelector(".working-process-description-1"),
+        {
+          type: "lines",
+          linesClass: "line",
+        },
+      );
+      const splitDesc2 = new SplitText(
+        container.current.querySelector(".working-process-description-2"),
+        {
+          type: "lines",
+          linesClass: "line",
+        },
+      );
+
+      // Wobble/shake animation
+      gsap.fromTo(
+        labelRef.current,
+        { rotate: "-2deg" },
+        {
+          rotation: "+=3", // Rotate 3 degrees back and forth
+          duration: 0.15, // Very short duration for quick wobble
+          yoyo: true, // Go back and forth
+          repeat: -1, // Infinite repeat
+          ease: "sine.inOut", // Best ease for wobble effects
+          repeatDelay: 0.5, // Small pause between wobbles
+        },
+      );
+
+      // Timeline for heading
+      const headingTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".working-process-heading",
+          start: "top 60%",
+          toggleActions: "play none none none",
+        },
+      });
+
+      headingTl.fromTo(
+        splitHeading.lines,
+        { opacity: 0, y: 40 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.4,
+          stagger: 0.2,
+          ease: "power2.out",
+        },
+        "<0.3",
+      );
+
+      // Timeline for description1
+      const descTl1 = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".working-process-description-1",
+          start: "top 60%",
+          toggleActions: "play none none none",
+        },
+      });
+
+      descTl1.fromTo(
+        splitDesc1.lines,
+        { opacity: 0, y: 40 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.4,
+          stagger: 0.2,
+          ease: "power2.out",
+        },
+        "<",
+      );
+
+      // Timeline for description2
+      const descTl2 = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".working-process-description-2",
+          start: "top 60%",
+          toggleActions: "play none none none",
+        },
+      });
+
+      descTl2.fromTo(
+        splitDesc2.lines,
+        { opacity: 0, y: 40 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.4,
+          stagger: 0.2,
+          ease: "power2.out",
+        },
+        "<",
+      );
+
+      // Timeline for Accordion
+      const accordionTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".working-process-accordion",
+          start: "top 60%",
+          toggleActions: "play none none none",
+        },
+      });
+
+      accordionTl.fromTo(
+        ".working-process-accordion",
+        { opacity: 0, y: 40 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.4,
+          stagger: 0.2,
+          ease: "power2.out",
+        },
+        "<",
+      );
+
+      // Independent Label Animation
+      gsap.fromTo(
+        labelRef.current,
+        { opacity: 0 },
+        {
+          opacity: 1,
+          duration: 0.4,
+          scrollTrigger: {
+            trigger: labelRef.current,
+            start: "top 60%",
+            toggleActions: "play none none none",
+          },
+        },
+      );
+
+      // Independent Cta Animation
+      gsap.fromTo(
+        ".working-process-cta",
+        { opacity: 0 },
+        {
+          opacity: 1,
+          duration: 0.4,
+          scrollTrigger: {
+            trigger: ".working-process-cta",
+            start: "top 60%",
+            toggleActions: "play none none none",
+          },
+        },
+      );
+    },
+    { scope: container },
+  );
 
   return (
-    <section className="relative px-[3rem] pt-[5rem] pb-[5rem] xl:px-[0rem] xl:pt-[14.7rem] xl:pb-[6.8rem]">
+    <section
+      ref={container}
+      className="relative px-[3rem] pt-[5rem] pb-[5rem] xl:px-[0rem] xl:pt-[14.7rem] xl:pb-[6.8rem]"
+    >
       <div className="pointer-events-none absolute inset-0 z-[0] select-none">
         <LineStroke25 className="absolute top-[50rem] left-1/2 w-full -translate-x-1/2" />
       </div>
 
       <div className="mx-auto max-w-[120rem]">
         <div className="flex flex-col items-center text-center">
-          <div ref={labelRef} className="rotate-[-2deg]">
+          <div ref={labelRef}>
             <SectionLabel2
               text="Our Working Process"
               bgColor="bg-[#FFC300]"
@@ -46,11 +198,11 @@ const WorkingProcessSection = () => {
             />
           </div>
 
-          <div className="mt-[2rem] mb-[3.6rem]">
+          <div className="working-process-heading mt-[2rem] mb-[3.6rem] overflow-hidden">
             <SectionTitle label="How We Work With Agencies" />
           </div>
 
-          <p className="max-w-[114.2rem] text-[2.2rem] leading-[3.2rem] font-medium text-[#070707]">
+          <p className="working-process-description-1 max-w-[114.2rem] overflow-hidden text-[2.2rem] leading-[3.2rem] font-medium text-[#070707]">
             Every agency works differently with their own systems, tools, and
             workflows. Some partner with us for{" "}
             <span className="text-[#FF37B3]">
@@ -70,7 +222,7 @@ const WorkingProcessSection = () => {
               <AccordionItem
                 key={item.id}
                 value={item.id}
-                className="!border-b border-[#9C9C9C] px-[2rem] py-[2rem] transition-all duration-200 data-[state=open]:rounded-[2rem] data-[state=open]:bg-[#FFE500] md:px-[2.8rem] md:py-[3rem]"
+                className="working-process-accordion !border-b border-[#9C9C9C] px-[2rem] py-[2rem] opacity-0 transition-all duration-200 data-[state=open]:rounded-[2rem] data-[state=open]:bg-[#FFE500] md:px-[2.8rem] md:py-[3rem]"
               >
                 <AccordionTrigger className="group flex w-full items-center justify-between">
                   <div className="flex items-center gap-[3rem] md:gap-[6rem]">
@@ -103,18 +255,20 @@ const WorkingProcessSection = () => {
         </div>
 
         <div className="mt-[5rem] flex flex-col items-center gap-[3rem] text-center">
-          <p className="text-[2.2rem] leading-[3.2rem] font-medium text-[#070707]">
+          <p className="working-process-description-2 overflow-hidden text-[2.2rem] leading-[3.2rem] font-medium text-[#070707]">
             With CreativePixels, the process isn't fixed - it's flexible. We're
             your white label agency partner, ready to scale design, development,
             or strategy support around your needs.
           </p>
 
-          <CommonBtn3
-            textColor="#FFFFFF"
-            bgColor="#FF37B3"
-            label="Get Free Consultation"
-            href="/contact"
-          />
+          <div className="working-process-cta">
+            <CommonBtn3
+              textColor="#FFFFFF"
+              bgColor="#FF37B3"
+              label="Get Free Consultation"
+              href="/contact"
+            />
+          </div>
         </div>
       </div>
     </section>
