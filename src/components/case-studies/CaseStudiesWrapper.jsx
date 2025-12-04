8ee6499145078a1eb3c2d89691c9c445c5c6993c
Taha Baig <taha.b@cp.agency>
2025-12-04 12:39:28 +0500

    all things delete in development branch

--- src/components/case-studies/CaseStudiesWrapper.jsx
+++
@@ -1,8 +0,0 @@
-// SERVER COMPONENT
-import { getCaseStudies } from "@/lib/strapi";
-
-export default async function CaseStudiesWrapper({ children }) {
-  const caseStudies = await getCaseStudies().catch(() => ({ data: [] }));
-
-  return <>{children(caseStudies)}</>;
-}
