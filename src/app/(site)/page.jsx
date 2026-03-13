2c361986d5e9dda236f8fe907e2997e1f3a08780
tahab-cp <taha.b@cp.agency>
2026-03-13 21:35:49 +0500

    og added in pages

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -4,6 +4,39 @@ import HomePage from "./home/HomePage";
 
 const options = { next: { revalidate: 30 } };
 
+export async function generateMetadata() {
+  const title = "CreativePixels | Web Design, WordPress & Branding Agency";
+  const description =
+    "CreativePixels is a Manchester-based creative agency delivering WordPress websites, branding, and digital solutions for clients across the UK, US & Australia.";
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: "https://creativepixels.agency/",
+      siteName: "CreativePixels",
+      images: [
+        {
+          url: "/images/og-image-assets/og-image.jpg",
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+      locale: "en_GB",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: ["/images/og-image-assets/og-image.jpg"],
+    },
+  };
+}
+
 const SitePage = async () => {
   const [caseStudies] = await Promise.all([
     caseStudiesClient.fetch(caseStudiesListingQuery, options),
