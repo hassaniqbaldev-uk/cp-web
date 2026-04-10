ffe3a24b02b6bb6a59f2a2fdf39d1020d20d26d9
Taha Baig <taha.b@cp.agency>
2026-04-10 22:05:41 +0500

    landing page seo no index added

--- src/app/lp/layout.jsx
+++ src/app/lp/layout.jsx
@@ -1,6 +1,10 @@
 import Footer from "@/components/layout/Footer";
 import LpHeader from "@/components/lp/LpHeader";
 
+export const metadata = {
+  robots: { index: false, follow: false },
+};
+
 export default function LandingPageLayout({ children }) {
   return (
     <>
